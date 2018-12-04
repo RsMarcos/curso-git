7c88217a92427c929b99f9a1a524a0bc267351ee
Marcos Roberto <rsmarcossilva@hotmail.com>
2018-12-03 22:38:44 -0200

    Projeto Iniciado, mostrando a mensagem ola mundo

---
+++ GuitTeste/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuitTeste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "Olá Mundo";
+            Console.ReadLine(s);
+        }
+    }
+}
